c5461f76f08495a6f35cebec17c7b1915dd63ff7
noyon674 <developernoyon9@gmail.com>
2024-09-13 15:13:12 +0600

    practiced about local module

---
+++ Lesson1/student.js
@@ -0,0 +1,12 @@
+const getName = ()=>{
+    console.log('Noyon Sarker')
+}
+
+const getAge = ()=>{
+    console.log(24)
+}
+
+module.exports = {
+    getName,
+    getAge
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+const s1 = require('./Lesson1/student')
+// you can also import both process
+const {getAge} = require('./Lesson1/student')
+
+s1.getName()
+s1.getAge()
+getAge()
