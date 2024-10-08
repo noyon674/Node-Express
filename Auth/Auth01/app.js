82a08120097bf4c286dbfeaa27dbeb59caf59d65
noyon674 <developernoyon9@gmail.com>
2024-10-08 19:26:20 +0600

    register is done

--- Auth/Auth01/app.js
+++ Auth/Auth01/app.js
@@ -33,7 +33,7 @@ app.post('/register', async (req, res)=>{
         await newUser.save()
         res.status(201).json(newUser)
     } catch (error) {
-        res.status(500).json(error.message)
+        res.status(500).json(error.message);
     }
 })
 
