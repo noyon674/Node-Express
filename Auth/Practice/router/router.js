4b6ba594818e93da9c7243eccc48398674b06b9d
noyon674 <developernoyon9@gmail.com>
2024-10-24 21:30:25 +0600

    write all the code of input validation

--- Auth/Practice/router/router.js
+++ Auth/Practice/router/router.js
@@ -1,7 +1,9 @@
 const { homeController, registerController } = require('../controllers/user.controller')
+const runValidation = require('../middleware/runValidation')
+const { registrationValidation } = require('../middleware/validation')
 const userRouter = require('express').Router()
 
 userRouter.get('/', homeController)
-userRouter.post('/register', registerController)
+userRouter.post('/register', registrationValidation, runValidation, registerController)
 
 module.exports = userRouter
